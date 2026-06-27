298976ae25d16645d9cd605b6adae922905b6f0a
Endlessblack <shutter211001@gmail.com>
2026-06-27 11:31:29 +0800

    feat: add scripts to fetch and log users from the ERP database

---
+++ check-erp-users.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from './node_modules/@shutter-erp/client';
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: 'postgresql://postgres:EgUCDEJmaPimGQGOZXPpnFWdBACqqMcS@reseau.proxy.rlwy.net:19511/railway'
+    }
+  }
+});
+
+async function main() {
+  const users = await prisma.user.findMany();
+  console.log('ERP Users:', users);
+}
+
+main().catch(console.error).finally(() => prisma.$disconnect());
