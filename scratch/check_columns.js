b6bf1c0009c76df40b61ecf0ca3e919791857d96
Endlessblack <shutter211001@gmail.com>
2026-07-19 09:44:29 +0800

    feat: implement configurable label printing module with customizable layouts and nutrition data management

--- scratch/check_columns.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const result = await prisma.$queryRaw`SELECT column_name FROM information_schema.columns WHERE table_name = 'orders'`;
-  console.log(JSON.stringify(result, null, 2));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
